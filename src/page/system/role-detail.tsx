2044ab5ec1fc318545f42a397a6afa5740bb52c9
weiwei <450552517@qq.com>
2022-04-20 12:05:50 +0800

    用户角色添加权限管理
    添加和编辑用户角色时 可以同时设置路由权限和操作权限

--- src/page/system/role-detail.tsx
+++ src/page/system/role-detail.tsx
@@ -1,7 +1,10 @@
+import UserPermissions from "@/component/UserPermissions";
 import router from "@/router";
+import { removeTab } from "@/service/common";
 import { defaultRole, getRoleDetail, IRole, postRole, putRole } from "@/service/role";
-import { Button, Form, FormItem, Input, Modal } from "ant-design-vue";
-import { defineComponent, onMounted, reactive } from "vue";
+import { Button, Form, FormItem, Input, Modal, Radio, RadioGroup } from "ant-design-vue";
+import { defineComponent, onMounted, reactive, ref } from "vue";
+import route from "./route";
 
 export default defineComponent({
   props: {
@@ -21,8 +24,17 @@ export default defineComponent({
       Modal.confirm({
         title: `确认${isAddPage ? "添加" : "编辑此"}角色？`,
         onOk: () => {
+          if (form.permissions) {
+            params.permissions = form.permissions
+          }
+          if (form.route) {
+            params.route = form.route
+          }
           return (isAddPage ? postRole({ ...params }) : putRole(form)).then(e => {
             router.back();
+            if (typeof route.name === 'string') {
+              removeTab(route.name)
+            }
           });
         },
       });
@@ -31,9 +43,13 @@ export default defineComponent({
     onMounted(() => {
       if (!isAddPage) {
         getRoleDetail(props.id).then(data => {
+          console.log(data)
           form.id = data.id;
           form.name = data.name;
           form.home_url = data.home_url;
+          form.status = data.status
+          form.permissions = data.permissions
+          form.route = data.route
         });
       }
     });
@@ -46,7 +62,30 @@ export default defineComponent({
         <FormItem name="home_url" label="首页地址">
           <Input placeholder="请输入首页地址" v-model={[form.home_url, "value"]}></Input>
         </FormItem>
-        <div class="d-flex align-items-center justify-center">
+        <FormItem name="status" label="是否激活" rules={[{ required: true, message: "请选择是否激活" }]}>
+          <RadioGroup v-model={[form.status, 'value']}>
+            <Radio value={1}>是</Radio>
+            <Radio value={0}>否</Radio>
+          </RadioGroup>
+        </FormItem>
+        <FormItem label="权限管理">
+          <UserPermissions
+            premissions = {form.permissions}
+            route = {form.route}
+            onPermissionKeyChange={ 
+              (item) => {
+                form.permissions = item
+              }
+            }
+            onRouteKeyChange= {
+              (item) => {
+                form.route = item
+              }
+            }
+          ></UserPermissions>
+        </FormItem>
+        
+        <div class="d-flex align-items-center justify-center mar-t-5">
           <Button htmlType="submit" type="primary" size="large">
             提交
           </Button>
