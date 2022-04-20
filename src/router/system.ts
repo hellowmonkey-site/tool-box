f33465fb4bbfd5bd87971fc4751d4f2c24f866cb
weiwei <450552517@qq.com>
2022-04-20 11:40:54 +0800

    系统设置路由
    系统设置功能 子模块功能路由

--- src/router/system.ts
+++ src/router/system.ts
@@ -6,7 +6,6 @@ const systemRoutes: RouteRecordRaw[] = [
     path: "/system/route",
     meta: {
       title: "页面设置",
-      keepAlive: true,
     },
     component: () => import("@/page/system/route"),
   },
@@ -15,6 +14,7 @@ const systemRoutes: RouteRecordRaw[] = [
     path: "/system/route/add",
     meta: {
       title: "添加页面",
+      keepAlive: true,
     },
     component: () => import("@/page/system/route-detail"),
   },
@@ -24,6 +24,7 @@ const systemRoutes: RouteRecordRaw[] = [
     props: true,
     meta: {
       title: "页面编辑",
+      keepAlive: true,
     },
     component: () => import("@/page/system/route-detail"),
   },
@@ -32,7 +33,6 @@ const systemRoutes: RouteRecordRaw[] = [
     path: "/system/role",
     meta: {
       title: "角色设置",
-      keepAlive: true,
     },
     component: () => import("@/page/system/role"),
   },
@@ -41,6 +41,7 @@ const systemRoutes: RouteRecordRaw[] = [
     path: "/system/role/add",
     meta: {
       title: "添加角色",
+      keepAlive: true,
     },
     component: () => import("@/page/system/role-detail"),
   },
@@ -50,6 +51,7 @@ const systemRoutes: RouteRecordRaw[] = [
     props: true,
     meta: {
       title: "角色编辑",
+      keepAlive: true,
     },
     component: () => import("@/page/system/role-detail"),
   },
@@ -58,7 +60,6 @@ const systemRoutes: RouteRecordRaw[] = [
     path: "/system/admin",
     meta: {
       title: "用户设置",
-      keepAlive: true,
     },
     component: () => import("@/page/system/admin"),
   },
@@ -67,6 +68,7 @@ const systemRoutes: RouteRecordRaw[] = [
     path: "/system/admin/add",
     meta: {
       title: "添加用户",
+      keepAlive: true,
     },
     component: () => import("@/page/system/admin-detail"),
   },
@@ -76,8 +78,117 @@ const systemRoutes: RouteRecordRaw[] = [
     props: true,
     meta: {
       title: "用户编辑",
+      keepAlive: true,
     },
     component: () => import("@/page/system/admin-detail"),
+  },
+  {
+    name: "system-banner",
+    path: "/system/banner",
+    meta: {
+      title: "幻灯设置",
+    },
+    component: () => import("@/page/system/banner"),
+  },
+  {
+    name: "system-banner-add",
+    path: "/system/banner/add",
+    meta: {
+      title: "添加幻灯",
+      keepAlive: true,
+    },
+    component: () => import("@/page/system/banner-detail"),
+  },
+  {
+    name: "system-banner-edit",
+    path: "/system/banner/:id(\\d+)",
+    props: true,
+    meta: {
+      title: "幻灯编辑",
+      keepAlive: true,
+    },
+    component: () => import("@/page/system/banner-detail"),
+  },
+  {
+    name: "system-module-index",
+    path: "/system/module",
+    meta: {
+      title: "模块设置",
+    },
+    component: () => import("@/page/system/module"),
+  },
+  {
+    name: "system-module-add",
+    path: "/system/module/add",
+    meta: {
+      title: "添加模块",
+      keepAlive: true,
+    },
+    component: () => import("@/page/system/module-detail"),
+  },
+  {
+    name: "system-module-edit",
+    path: "/system/module/:id(\\d+)",
+    props: true,
+    meta: {
+      title: "模块编辑",
+      keepAlive: true,
+    },
+    component: () => import("@/page/system/module-detail"),
+  },
+  {
+    name: "system-permission-index",
+    path: "/system/permission",
+    meta: {
+      title: "权限设置",
+    },
+    component: () => import("@/page/system/permission"),
+  },
+  {
+    name: "system-permission-add",
+    path: "/system/permission/add",
+    meta: {
+      title: "添加权限",
+      keepAlive: true,
+    },
+    component: () => import("@/page/system/permission-detail"),
+  },
+  {
+    name: "system-permission-edit",
+    path: "/system/permission/:id(\\d+)",
+    props: true,
+    meta: {
+      title: "权限编辑",
+      keepAlive: true,
+    },
+    component: () => import("@/page/system/permission-detail"),
+  },
+  {
+    name: "system-app-index",
+    path: "/system/app",
+    meta: {
+      title: "APP设置",
+    },
+    component: () => import("@/page/system/app"),
+  },
+  {
+    name: "system-app-add",
+    path: "/system/app/add",
+    meta: {
+      title: "添加APP",
+      keepAlive: true,
+    },
+    component: () => import("@/page/system/app-detail"),
+  },
+  {
+    name: "system-app-edit",
+    path: "/system/app/:id(\\d+)",
+    props: true,
+    meta: {
+      title: "APP编辑",
+      keepAlive: true,
+    },
+    component: () => import("@/page/system/app-detail"),
   },
 ];
 export default systemRoutes;
