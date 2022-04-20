528fd67740b61c153d4acf07a55b9c6d2e03219f
weiwei <450552517@qq.com>
2022-04-20 11:35:40 +0800

    APP管理
    添加 APP管理功能

---
+++ src/page/system/app-detail.tsx
@@ -0,0 +1,72 @@
+import router from "@/router";
+import { defaultApp, getAppDetail, IApp, postApp, putApp } from "@/service/app";
+import { removeTab } from "@/service/common";
+import { Button, Form, FormItem, Input, Modal, Switch } from "ant-design-vue";
+import { defineComponent, onMounted, reactive } from "vue";
+import { useRoute } from "vue-router";
+
+export default defineComponent({
+  props: {
+    id: {
+      type: Number,
+      default: null,
+    },
+  },
+  emits: [],
+  setup: (props, ctx) => {
+    const form = reactive<IApp>({
+      ...defaultApp,
+    });
+    const isAddPage = props.id === null;
+    const route = useRoute();
+
+    const handleSubmit = (params: IApp) => {
+      Modal.confirm({
+        title: `确认${isAddPage ? "添加" : "编辑此"}模块？`,
+        onOk: () => {
+          return (isAddPage ? postApp({ ...params }) : putApp(form)).then(e => {
+            router.back();
+            if (typeof route.name === 'string') {
+              removeTab(route.name)
+            }
+          });
+        },
+      });
+    };
+
+    onMounted(() => {
+      if (!isAddPage) {
+        getAppDetail(props.id).then(data => {
+          form.id = data.id;
+          form.name = data.name;
+          form.app_key = data.app_key;
+          form.secret_key = data.secret_key;
+          form.status = data.status
+        });
+      }
+    });
+
+    return () => (
+      <Form model={form} labelCol={{ sm: 4 }} onFinish={e => handleSubmit(e)}>
+        <FormItem name="name" label="APP名称" rules={[{ required: true, message: "请先输入APP名称" }]}>
+          <Input placeholder="请输入APP名称" v-model={[form.name, "value"]}></Input>
+        </FormItem>
+        <FormItem name="app_key" label="app_key">
+          <Input placeholder="请输入app_key" v-model={[form.app_key, "value"]}></Input>
+        </FormItem>
+        <FormItem name="secret_key" label="secret_key">
+          <Input placeholder="请输入secret_key" v-model={[form.secret_key, "value"]}></Input>
+        </FormItem>
+        <FormItem name="status" label="是否启用" rules={[{ required: false, message: "请先选择是不是集团用户" }]}>
+          <Switch v-model={[form.status, "checked"]} checkedChildren="是" unCheckedChildren="否" unCheckedValue={0} checkedValue={1} />
+        </FormItem>
+        <div class="d-flex align-items-center justify-center">
+          <Button htmlType="submit" type="primary" size="large">
+            提交
+          </Button>
+        </div>
+      </Form>
+    );
+  },
+});
+
---
+++ src/page/system/app.tsx
@@ -0,0 +1,97 @@
+import { TableData } from "@/config/type";
+import { getAppList, IApp, appList, deleteApp } from "@/service/app";
+import { Button, message, Modal, Table } from "ant-design-vue";
+import { defineComponent, onMounted, ref } from "vue";
+import { RouterLink } from "vue-router";
+
+export default defineComponent({
+  props: {},
+  emits: [],
+  setup: (props, ctx) => {
+    const dataSource = ref<IApp[]>([]);
+    const columns = [
+      {
+        dataIndex: "id",
+        title: "ID",
+      },
+      {
+        dataIndex: "name",
+        title: "app名称",
+      },
+      {
+        dataIndex: "app_key",
+        title: "app_key",
+      },
+      {
+        dataIndex: "secret_key",
+        title: "secret_key",
+      },
+      {
+        dataIndex: "status",
+        title: "状态",
+        customRender({ record }: TableData) {
+          return (
+            <>
+              {record.status===1?'启用':'停用'}
+            </>
+          );
+        },
+      },
+      {
+        key: "action",
+        title: "操作",
+        customRender({ record }: TableData) {
+          return (
+            <>
+              <RouterLink to={{ name: "system-app-edit", params: { id: record.id } }} class="mar-r-2-item ant-btn ant-btn-primary">
+                编辑
+              </RouterLink>
+              <Button
+                class="mar-r-2-item"
+                danger
+                onClick={() => {
+                  Modal.confirm({
+                    title: `确认要删除${record.title}吗？`,
+                    onOk: () => {
+                      return deleteApp(record.id).then(() => {
+                        fetchData();
+                      });
+                    },
+                  });
+                }}
+              >
+                删除
+              </Button>
+            </>
+          );
+        },
+      },
+    ];
+
+    function fetchData() {
+      const hide = message.loading("数据加载中...");
+      getAppList()
+        .then(data => {
+          dataSource.value = appList.value;
+        })
+        .finally(() => {
+          hide();
+        });
+    }
+
+    onMounted(() => {
+      fetchData();
+    });
+
+    return () => (
+      <>
+        <div class="d-flex justify-end mar-b-3">
+          <RouterLink to={{ name: "system-app-add" }} class="ant-btn">
+            添加
+          </RouterLink>
+        </div>
+        <Table bordered columns={columns} pagination={false} dataSource={dataSource.value}></Table>
+      </>
+    );
+  },
+});
