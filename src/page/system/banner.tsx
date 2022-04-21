dd7a2e112d3fff5de52fe66aba4d19bc95eaec64
weiwei <450552517@qq.com>
2022-04-21 09:02:00 +0800

    幻灯列表调整
    幻灯列表调整

--- src/page/system/banner.tsx
+++ src/page/system/banner.tsx
@@ -1,11 +1,10 @@
-import OperationTip from "@/component/OperationTip";
 import { TableData } from "@/config/type";
 import router from "@/router";
-import { deleteBanner, IBananer, putBanner } from "@/service/banner";
-import { PlusOutlined } from "@ant-design/icons-vue";
-import { Button, message, Modal, Table, Breadcrumb, BreadcrumbItem, Pagination, Tabs, TabPane, Input, Form, DatePicker } from "ant-design-vue";
+import { defaultBanner, deleteBanner, getBannerList, IBananer, putBanner } from "@/service/banner";
+import { Button, Image, Modal, Table, Pagination, Tabs, TabPane, Input, Form, DatePicker, message, FormItem } from "ant-design-vue";
+import { Key } from "ant-design-vue/lib/_util/type";
 import { defineComponent, onMounted, ref } from "vue";
-import { RouterLink, useRouter } from "vue-router";
+import { onBeforeRouteUpdate, RouterLink, useRouter } from "vue-router";
 
 export default defineComponent({
   props: {},
@@ -14,49 +13,40 @@ export default defineComponent({
     const dataSource = ref<IBananer[]>([]);
     const route = useRouter()
 
-    function handleSubmit(e: Event) {
-      console.log(e)
-    }
-    function fetchData() {
-
-    }
-
     const columns = [
       {
         dataIndex: "id",
         title: "ID",
       },
       {
-        dataIndex: "title",
+        dataIndex: "name",
         title: "标题",
-      },
-      {
-        dataIndex: "content",
-        title: "内容",
-      },
-      {
-        dataIndex: "type",
-        title: "方式",
-      },
-      {
-        dataIndex: "way",
-        title: "投放终端",
-      },
-      {
-        dataIndex: "pos",
-        title: "投放位置",
       },
       {
         dataIndex: "pic",
         title: "图片",
+        customRender({ record }: TableData) {
+          return (
+            <>
+              <Image width={80} height={80} src={record.img_path} placeholder={true}></Image>
+            </>
+          );
+        },
       },
       {
-        dataIndex: "sort",
-        title: "排序",
+        dataIndex: "url",
+        title: "url",
       },
       {
         dataIndex: "status",
         title: "状态",
+        customRender({ record }: TableData) {
+          return (
+            <>
+              {record.status===1?"上线":"下线"}
+            </>
+          );
+        },
       },
       {
         key: "action",
@@ -64,44 +54,39 @@ export default defineComponent({
         customRender({ record }: TableData) {
           return (
             <>
-              <Button
+            <RouterLink to={{ name: "system-banner-edit", params: { id: record.id } }} class="mar-r-2-item ant-btn ant-btn-primary">
+             编辑
+            </RouterLink>
+            <Button
                 class="mar-r-2-item"
-                danger
                 onClick={() => {
                   Modal.confirm({
-                    title: `确认要上线${record.title}吗？`,
+                    title: `确认要${record.status===1?"下线":"上线"}${record.name}吗？`,
                     onOk: () => {
-                      return putBanner(record.id).then(() => {
-                        fetchData();
-                      });
+                      const data = dataSource.value.filter(item=> item.id===record.id)
+                      if (data && data.length>0) {
+                        const sendItem: IBananer = defaultBanner;
+                        sendItem.id = data[0].id 
+                        sendItem.name = data[0].name
+                        sendItem.img_path = data[0].img_id || ""
+                        sendItem.url = sendItem.url || ""
+                        sendItem.status = data[0].status===1?0:1
+                        return putBanner(sendItem).then(() => {
+                          fetchData();
+                        });
+                      }
                     },
                   });
                 }}
               >
-                上线
+                {record.status===1?"下线":"上线"}
               </Button>
               <Button
                 class="mar-r-2-item"
                 danger
                 onClick={() => {
                   Modal.confirm({
-                    title: `确认要下线${record.title}吗？`,
-                    onOk: () => {
-                      return putBanner(record.id).then(() => {
-                        fetchData();
-                      });
-                    },
-                  });
-                }}
-              >
-                下线
-              </Button>
-              <Button
-                class="mar-r-2-item"
-                danger
-                onClick={() => {
-                  Modal.confirm({
-                    title: `确认要删除${record.title}吗？`,
+                    title: `确认要删除${record.name}吗？`,
                     onOk: () => {
                       return deleteBanner(record.id).then(() => {
                         fetchData();
@@ -118,38 +103,42 @@ export default defineComponent({
       },
     ];
 
+    function fetchData() {
+      const hide = message.loading("数据加载中...");
+      getBannerList()
+        .then(data => {
+          dataSource.value = data;
+        })
+        .finally(() => {
+          hide();
+        });
+    }
+
+    onBeforeRouteUpdate(e => {
+      fetchData();
+    });
+
+    onMounted(() => {
+      fetchData();
+    });
+
+    // function clickTab(e: Key) {
+    //   fetchData()
+    // }
+
     return () => (
       <div>
-        <div class='d-flex justify-between direction-row align-items-center'>
-          <Tabs class="flex-item-extend">
+        <div class='d-flex justify-end direction-row align-items-center'>
+          {/* <Tabs class="flex-item-extend" onTabClick={ e => { fetchData(e) } }>
             <TabPane key="1" tab="幻灯片列表">
             </TabPane>
             <TabPane key="2" tab="已上线"></TabPane>
             <TabPane key="3" tab="已下线"></TabPane>
-          </Tabs>
+          </Tabs> */}
           <RouterLink to={{ name: "system-banner-add" }} class="mar-l-5 mar-b-5 ant-btn ant-btn-primary">
                 新 增
           </RouterLink>
         </div>
-          <div>
-            
-          </div>
-          <Form class="d-block" onSubmit={e=>{ handleSubmit(e) }}>
-            <div class="d-flex justify-between direction-row align-items-start border pad-3">
-              <div class="flex-item-extend wrap d-flex justify-start direction-row align-items-start">
-                <div class="wrap-nowrap d-flex justify-start direction-row align-items-center">
-                  <span class="space-nowrap mar-r-3">幻灯标题</span>
-                  <Input></Input>
-                </div>
-                <div class="wrap-nowrap mar-l-5 d-flex justify-start direction-row align-items-center">
-                  <span class="space-nowrap mar-r-3 mar-l-3">创建日期</span>
-                  <DatePicker></DatePicker>
-                </div>
-              </div>
-              <Button type="primary" class="font-light mar-l-3">查询</Button>
-            </div>
-          </Form>
-        
         <Table columns={columns} pagination={false} dataSource={dataSource.value}>
         </Table>
         <div class="d-flex direction-row justify-end mar-t-5">
