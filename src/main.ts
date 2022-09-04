44f13ab0539ee806de6c2052c2d713d296d6c7d0
zhanglei <631960109@qq.com>
2022-09-04 23:06:20 +0800

    feat：初始化

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,14 @@
 import { createApp } from "vue";
 import App from "./layout/App";
 import router from "@/router";
-import "ant-design-vue/dist/antd.less";
 import "@/static/style/app.scss";
 
 import "@/extend";
-import "@/plugin/dayjs";
-import "@/plugin/ajax";
-import { getUserInfo, userToken } from "./service/user";
 
 const app = createApp(App);
 
 app.use(router);
 
 router.isReady().then(async () => {
-  if (userToken.value) {
-    await getUserInfo();
-  }
   app.mount("#app");
 });
